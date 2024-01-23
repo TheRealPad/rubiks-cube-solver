057d9804cf5a4d17d3f981feabf987569af3e4c9
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-01-22 16:49:09 -0800

    test(unit test): add unit test for solver and peripheral

--- src/Solver/Peripherals.rs
+++ src/Solver/Peripherals.rs
@@ -25,3 +25,38 @@ impl Peripherals {
 pub static mut PERIPHERALS: Peripherals = Peripherals {
     solver: None,
 };
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_take_serial_fridrich() {
+        unsafe {
+            PERIPHERALS.solver = None;
+        }
+        let solver = unsafe { PERIPHERALS.take_serial("fridrich".to_string()) };
+
+        assert_eq!(solver.get_description(), "The Fridrich method");
+    }
+
+    #[test]
+    fn test_take_serial_roux() {
+        unsafe {
+            PERIPHERALS.solver = None;
+        }
+        let solver = unsafe { PERIPHERALS.take_serial("roux".to_string()) };
+
+        assert_eq!(solver.get_description(), "The method roux");
+    }
+
+    #[test]
+    fn test_take_serial_default() {
+        unsafe {
+            PERIPHERALS.solver = None;
+        }
+        let solver = unsafe { PERIPHERALS.take_serial("unknown_method".to_string()) };
+
+        assert_eq!(solver.get_description(), "The Fridrich method");
+    }
+}
--- src/Solver/Solver.rs
+++ src/Solver/Solver.rs
@@ -1,3 +1,19 @@
+/// Interface Solver
 pub trait Solver {
+    /// return the description of the selected solver
     fn get_description(& self) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solver::fridrich::Fridrich::Fridrich;
+    use super::*;
+
+    #[test]
+    fn test_example_solver_description() {
+        let example_solver: &dyn Solver = &Fridrich {};
+        let description = example_solver.get_description();
+
+        assert_eq!(description, "The Fridrich method");
+    }
 }
