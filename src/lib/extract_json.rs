7f396a9d182871f3dfb43c34736ee8a652428843
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-01-23 16:22:51 -0800

    feat(read files): function for read files
    - extract json
    - json files for init cubes

---
+++ src/lib/extract_json.rs
@@ -0,0 +1,85 @@
+use std::error::Error;
+use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize)]
+pub struct CubeFace {
+    pub name: String,
+    pub rows: Vec<Vec<String>>,
+}
+
+pub fn extract_json<T>(json: &str) -> Result<T, Box<dyn Error>>
+    where
+        T: DeserializeOwned,
+{
+    let json_value: Value = serde_json::from_str(json)?;
+    let v: T = serde_json::from_value(json_value)?;
+
+    Ok(v)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_json_cube_face() {
+        let json_data = r#"
+            [
+              {
+                "name": "Down",
+                "rows": [
+                  ["W", "W", "W"],
+                  ["W", "W", "W"],
+                  ["W", "W", "W"]
+                ]
+              },
+              {
+                "name": "Left",
+                "rows": [
+                  ["R", "R", "R"],
+                  ["R", "R", "R"],
+                  ["R", "R", "R"]
+                ]
+              },
+              {
+                "name": "Front",
+                "rows": [
+                  ["B", "B", "B"],
+                  ["B", "B", "B"],
+                  ["B", "B", "B"]
+                ]
+              },
+              {
+                "name": "Right",
+                "rows": [
+                  ["O", "O", "O"],
+                  ["O", "O", "O"],
+                  ["O", "O", "O"]
+                ]
+              },
+              {
+                "name": "Back",
+                "rows": [
+                  ["G", "G", "G"],
+                  ["G", "G", "G"],
+                  ["G", "G", "G"]
+                ]
+              },
+              {
+                "name": "Top",
+                "rows": [
+                  ["Y", "Y", "Y"],
+                  ["Y", "Y", "Y"],
+                  ["Y", "Y", "Y"]
+                ]
+              }
+            ]
+        "#;
+
+        let result: Result<Vec<CubeFace>, Box<dyn Error>> = extract_json(json_data);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().len(), 6);
+    }
+}
--- src/rubiks_cube_solver.rs
+++ src/rubiks_cube_solver.rs
@@ -1,6 +1,8 @@
 use std::panic;
 use crate::Cube::Cube::Cube;
 use crate::Face::Face::{CaseColor, Face, FacePosition};
+use crate::lib::extract_json::{CubeFace, extract_json};
+use crate::lib::read_file::get_file_content;
 use crate::Solver::Peripherals::PERIPHERALS;
 use crate::Solver::Solver::Solver;
 
@@ -14,25 +16,55 @@ fn error_handling(args: &Vec<String>) -> bool {
         println!("ARGUMENTS");
         println!("\t--a [name]: run the program with a selected algorithm");
         println!("\t\t* Fridrich (if cannot find the name, default method)");
+        println!("\t--f [path]: path to the file containing the initial state of the cube");
         return true;
     }
-    if args.len() < 3 {
+    if args.len() < 5 {
         panic!("not enough parameters")
     }
     return false
+}
+
+fn extract_arguments(args: &[String]) -> (String, String) {
+    let mut algorithm = "".to_string();
+    let mut file_path = "".to_string();
+    let mut iter = args.iter().enumerate().skip(1);
+
+    while let Some(arg) = iter.next() {
+        match arg.1.as_str() {
+            "--a" => {
+                if arg.0 < args.len() -1 {
+                    algorithm = args.get(arg.0 + 1).unwrap().to_string()
+                }
+            },
+            "--f" => {
+                if arg.0 < args.len() -1 {
+                    file_path = args.get(arg.0 + 1).unwrap().to_string()
+                }
+            },
+            _ => {}
+        }
+    }
+    (algorithm, file_path)
 }
 
 pub fn rubiks_cube_solver(args: Vec<String>) {
     if error_handling(&args) {
         return;
     }
-    let result_solver = panic::catch_unwind(|| unsafe { PERIPHERALS.take_serial(args.get(2).unwrap().to_string()) });
+    let (algorithm, file_path) = extract_arguments(&args);
+    let v = extract_json::<Vec<CubeFace>>(get_file_content(file_path.to_string()).as_str());
+    match v {
+        Ok(_) => {}
+        Err(e) => return
+    }
+    let result_solver = panic::catch_unwind(|| unsafe { PERIPHERALS.take_serial(algorithm) });
 
     if result_solver.is_err() {
         println!("cannot create 2 instances of Solver");
         return;
     }
-    let mut solver = result_solver.unwrap();
+    /* let mut solver = result_solver.unwrap();
     solver.set_cube(Cube::new(vec![
         Face::new(CaseColor::White, FacePosition::Top),
         Face::new(CaseColor::Yellow, FacePosition::Down),
@@ -45,5 +77,5 @@ pub fn rubiks_cube_solver(args: Vec<String>) {
     solver.resolve();
     let cubeB = solver.get_cube();
     println!("{:?}", cubeB.get_face(FacePosition::Front));
-    println!("{:?}", solver.get_movements());
+    println!("{:?}", solver.get_movements()); */
 }
